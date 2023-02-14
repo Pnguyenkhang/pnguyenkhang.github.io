8f8d27d6a7d04648d5f88ba689f5982dad0f7760
peter <pnguyenkhang@gmail.com>
2023-02-13 23:25:48 -0800

    added more touches

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
-const ghost1 = document.getElementById("ghost1");
+const character = document.getElementById("character");
 const ghost2 = document.getElementById("ghost2");
 const boundary = {
     left: 0,
-    right: window.innerWidth - ghost1.offsetWidth,
+    right: window.innerWidth - character.offsetWidth,
     top: 0,
-    bottom: window.innerHeight - ghost1.offsetHeight
+    bottom: window.innerHeight - character.offsetHeight
 };
       
 document.addEventListener("keydown", function(event) {
-    let currentTop1 = ghost1.offsetTop;
-    let currentLeft1 = ghost1.offsetLeft;
+    let currentTop1 = character.offsetTop;
+    let currentLeft1 = character.offsetLeft;
     let currentTop2 = ghost2.offsetTop;
     let currentLeft2 = ghost2.offsetLeft;
 
     switch (event.key) {
         case "w":
         currentTop1 = Math.max(boundary.top, currentTop1 - 10);
-        ghost1.style.top = currentTop1 - 40 + "px";
+        character.style.top = currentTop1 - 5 + "px";
+        character.src = "kelseyback.png"
+        character.style.width = "27px";
         break;
         case "s":
         currentTop1 = Math.min(boundary.bottom, currentTop1 + 10);
-        ghost1.style.top = currentTop1 + 40 + "px";
+        character.style.top = currentTop1 + 5 + "px";
+        character.src = "kelseyfront.png";
+        character.style.width = "27px";
         break;
         case "a":
         currentLeft1 = Math.max(boundary.left, currentLeft1 - 10);
-        ghost1.style.left = currentLeft1 - 40 + "px";
-        ghost1.style.transform = "scaleX(-1)";
+        character.style.left = currentLeft1 - 5 + "px";
+        character.src = "kelseyleft.png";
+        character.style.width = "25px";
         break;
         case "d":
         currentLeft1 = Math.min(boundary.right, currentLeft1 + 10);
-        ghost1.style.left = currentLeft1 + 40 + "px";
-        ghost1.style.transform = "scaleX(1)";
+        character.style.left = currentLeft1 + 5 + "px";
+        character.src = "kelseyright.png";
+        character.style.width = "25px";
         break;
         case "ArrowUp":
         currentTop2 = Math.max(boundary.top, currentTop2 - 10);
